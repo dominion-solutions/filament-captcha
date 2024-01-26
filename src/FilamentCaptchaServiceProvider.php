544be6ca0b353c3b39045f07f6f3492bb890354c
spam-n-eggs <spam-n-eggs@users.noreply.github.com>
2024-01-26 01:39:10 +0000

    Fix styling

--- src/FilamentCaptchaServiceProvider.php
+++ src/FilamentCaptchaServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace DominionSolutions\FilamentCaptcha;
 
+use DominionSolutions\FilamentCaptcha\Commands\FilamentCaptchaCommand;
+use DominionSolutions\FilamentCaptcha\Testing\TestsFilamentCaptcha;
 use Filament\Support\Assets\AlpineComponent;
 use Filament\Support\Assets\Asset;
 use Filament\Support\Assets\Css;
@@ -13,8 +15,6 @@ use Livewire\Features\SupportTesting\Testable;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use DominionSolutions\FilamentCaptcha\Commands\FilamentCaptchaCommand;
-use DominionSolutions\FilamentCaptcha\Testing\TestsFilamentCaptcha;
 
 class FilamentCaptchaServiceProvider extends PackageServiceProvider
 {
