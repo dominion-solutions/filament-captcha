8095aa8b54bd36acd97e052cce4c006a9ea53dcb
Mark Horninger <mark.horninger@dominion.solutions>
2026-07-26 21:50:36 -0400

    Feature/modernization laravel 13 (#26)
    * Bump stefanzweifel/git-auto-commit-action from 5 to 7
    
    Bumps [stefanzweifel/git-auto-commit-action](https://github.com/stefanzweifel/git-auto-commit-action) from 5 to 7.
    - [Release notes](https://github.com/stefanzweifel/git-auto-commit-action/releases)
    - [Changelog](https://github.com/stefanzweifel/git-auto-commit-action/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/stefanzweifel/git-auto-commit-action/compare/v5...v7)
    
    ---
    updated-dependencies:
    - dependency-name: stefanzweifel/git-auto-commit-action
      dependency-version: '7'
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump actions/checkout from 4 to 6
    
    Bumps [actions/checkout](https://github.com/actions/checkout) from 4 to 6.
    - [Release notes](https://github.com/actions/checkout/releases)
    - [Changelog](https://github.com/actions/checkout/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/actions/checkout/compare/v4...v6)
    
    ---
    updated-dependencies:
    - dependency-name: actions/checkout
      dependency-version: '6'
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump dependabot/fetch-metadata from 2.4.0 to 2.5.0
    
    Bumps [dependabot/fetch-metadata](https://github.com/dependabot/fetch-metadata) from 2.4.0 to 2.5.0.
    - [Release notes](https://github.com/dependabot/fetch-metadata/releases)
    - [Commits](https://github.com/dependabot/fetch-metadata/compare/v2.4.0...v2.5.0)
    
    ---
    updated-dependencies:
    - dependency-name: dependabot/fetch-metadata
      dependency-version: 2.5.0
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Updated to support the existing latest versions of PHP, Laravel, etc.
    
    * Updated test matrix.
    
    * Fixed the matrix
    
    * Updates to support older php versions
    
    * Added the exclude block.
    
    * Updated the application so pcov is the coverage driver
    
    * Removed Ray from it.
    
    * Fixed dependencies and larastan
    
    * Fix styling
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/FilamentCaptcha.php
+++ src/FilamentCaptcha.php
@@ -2,6 +2,4 @@
 
 namespace DominionSolutions\FilamentCaptcha;
 
-class FilamentCaptcha
-{
-}
+class FilamentCaptcha {}
--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -1,5 +1,35 @@
 <?php
 
+use DominionSolutions\FilamentCaptcha\Commands\FilamentCaptchaCommand;
+use DominionSolutions\FilamentCaptcha\FilamentCaptcha;
+use DominionSolutions\FilamentCaptcha\FilamentCaptchaServiceProvider;
+use DominionSolutions\FilamentCaptcha\Forms\Components\Captcha;
+use DominionSolutions\FilamentCaptcha\Testing\TestsFilamentCaptcha;
+
 it('will not use debugging functions')
-    ->expect(['dd', 'dump', 'ray'])
+    ->expect(['dd', 'dump', 'ray', 'var_dump', 'print_r'])
     ->each->not->toBeUsed();
+
+it('will not use exit or die')
+    ->expect(['exit', 'die'])
+    ->each->not->toBeUsed();
+
+it('will not use logging helpers')
+    ->expect(['error_log'])
+    ->each->not->toBeUsed();
+
+it('has no direct Log facade usage in source files')
+    ->expect(['Illuminate\Support\Facades\Log'])
+    ->each->not->toBeUsed();
+
+it('only uses classes in the correct root namespace')
+    ->expect([
+        FilamentCaptcha::class,
+        FilamentCaptchaServiceProvider::class,
+        DominionSolutions\FilamentCaptcha\Facades\FilamentCaptcha::class,
+        Captcha::class,
+        FilamentCaptchaCommand::class,
+        TestsFilamentCaptcha::class,
+    ])
+    ->each
+    ->toContain('DominionSolutions\\FilamentCaptcha');
